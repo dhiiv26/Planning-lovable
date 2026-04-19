fa79cefc3c6f82a67e739449c8d49be10548251b
Adri <dhiiv26@gmail.com>
2026-04-19 16:42:50 +0200

    update

---
+++ src/config/version.ts
@@ -0,0 +1,3 @@
+// Version locale de l'application. Incrémenter à chaque release.
+export const APP_VERSION = '1.0.0';
+export const VERSION_CHECK_URL = 'https://apkplanning.infinityfreeapp.com/version.json';
